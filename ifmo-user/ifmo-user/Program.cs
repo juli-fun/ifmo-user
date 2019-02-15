935274a8048bd41afc82cac45082d4a697cf3f1d
Julia Anikienko <juli-fun@Ramils-MacBook-Pro.local>
2019-02-15 20:50:14 +0300

    Just created a project in Visual Studio

---
+++ ifmo-user/ifmo-user/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ifmo_user
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
