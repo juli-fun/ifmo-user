2505ddaef51844f2b936ca6a06f695333008963b
Julia Anikienko <juli-fun@Ramils-MacBook-Pro.local>
2019-02-15 22:10:34 +0300

    Начинаем создавать структуру

--- ifmo-user/ifmo-user/Program.cs
+++ ifmo-user/ifmo-user/Program.cs
@@ -6,7 +6,37 @@ namespace ifmo_user
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Создаем тестового пользователя
+            var User1 = new User("berezhkov@ifmo.ru", "P@ssw0rd_123",
+            "berezhkov", "Бережков", "Андрей", "Вячеславович");
         }
+    }
+
+    public class User
+    {
+        public string email, login, password, fname, name, lname, auth_key;
+        public DateTime created_at, updated_at, birth_date;
+        public bool is_blocked;
+
+        // Cоздание пользователя
+        // Параметры, которые помечены звездочкой (*), - обязательные,
+        // также обязательный сделан логин. Остальные - необязательные
+        public User (string _email, string _password, string _login,
+            string _fname = "Surname", string _name = "Name",
+            string _lname = "Middlename")
+        {
+
+        }
+
+        // Eдаление пользователя
+        ~User()
+        {
+
+        }
+
+        // Вместо создания метода обновления пользователя, мы сделали
+        // редактируемыми опции объекта 
+
+        public 
     }
 }
